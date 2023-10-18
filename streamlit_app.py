9b287be231082fbd36bbe31dbf47d24a3e2e252d
yk-ky-yk <146862041+yk-ky-yk@users.noreply.github.com>
2023-10-18 12:17:02 -0400

    Update streamlit_app.py
    Fixing indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-    my_cur.execute("Select * from fruit_load_list")
-    return my_cur.fetchall()
+        my_cur.execute("Select * from fruit_load_list")
+        return my_cur.fetchall()
 
 # Add a button to laod the fruit
 if streamlit.button('Get Fruit Load List'):
